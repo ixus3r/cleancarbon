db01e9f8ac8bb3c1da754b81ee8bc5585369b44d
ixus3r <redbullex@gmail.com>
2016-07-02 07:25:23 +0300

    1.01
    add Laravel to project

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Http\Controllers;
+
+/**
+ * Description of PageController
+ *
+ * @author ixus3r
+ */
+class PageController {
+    // Add methods to add, edit, delete and show pages
+    // create method to create new pages
+    // submit the form to this method
+    
+    // Show a page by slug
+    public function show($slug = 'home')
+    {
+        echo "<pre>";
+        print_r($slug);
+        echo "</pre>";
+       
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller {
+
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function showProfile($id) {
+       return $id;
+    }
+    
+    public function home(){
+        return 'hello';
+    }
+
+}
